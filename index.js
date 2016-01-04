tbranyen/salita

commit a645b396823d1808d6d59bc6d6bebeda4889f020

Add support for `--json` option.

bin/salita
@@ -2,13 +2,16 @@
 
 var minimist = require('minimist');
 var options = minimist(process.argv, {
-  boolean: ['no-color'],
+  boolean: ['no-color', 'json'],
   default: {
-    'no-color': false
+    'no-color': false,
+    json: false
   }
 });
 var salita = require("../");
 
 salita(process.cwd(), options, function() {
-  console.log("Completed upgrade.");
+  if (!options.json) {
+    console.log("Completed upgrade.");
+  }
 });

index.js
@@ -5,6 +5,7 @@ var jsonFile = require("json-file-plus");
 var Table = require("cli-table");
 var chalk = require("chalk");
 var Promise = require('promise');
+var assign = require('object.assign');
 
 var getTable = function () {
   return new Table({
@@ -26,6 +27,14 @@ var getTable = function () {
       "middle": ""
     }
   });
+};
+
+var createResultJSON = function (key) {
+  return function (results) {
+    var obj = {};
+    obj[key] = results;
+    return obj;
+  };
 };
 
 var createResultTable = function (caption) {
@@ -78,30 +87,35 @@ var isLoaded = false;
  * The main entry point.
  */
 function salita(dir, options, callback) {
+  chalk.enabled = !options['no-color'] && !options.json;
   // Package.json.
   var filename = path.join(dir, 'package.json');
   jsonFile(filename, function (err, pkg) {
     if (err) { throw err; }
 
-    if (pkg) {
+    if (pkg && !options.json) {
       console.log("Found package.json.");
     }
 
     // Check all the dependencies.
     var deps = Promise.all(dependenciesLookup(pkg.data, "dependencies"))
-      .then(createResultTable('Dependencies'));
+      .then(options.json ? createResultJSON('dependencies') : createResultTable('Dependencies'));
 
     // Check all the devDependencies.
     var devDeps = Promise.all(dependenciesLookup(pkg.data, "devDependencies"))
-      .then(createResultTable('Development Dependencies'));
+      .then(options.json ? createResultJSON('devDependencies') : createResultTable('Development Dependencies'));
 
     // Wait for all of them to resolve.
     Promise.all([deps, devDeps]).then(function (depResults) {
-      depResults.forEach(function (results) {
-        results.map(String).forEach(function (result) {
-          console.log(result);
+      if (options.json) {
+        console.log(JSON.stringify(assign.apply(null, [{}].concat(depResults))));
+      } else {
+        depResults.forEach(function (results) {
+          results.map(String).forEach(function (result) {
+            console.log(result);
+          });
         });
-      });
+      }
 
       // Write back the package.json.
       pkg.save(callback);

commit e2132b431be5ea18f158c51511a32741a7515625

[Fix] avoid trumping `—no-color` option.
Broken in a645b396

Fixes #8.

bin/salita
@@ -5,7 +5,7 @@
 var options = require('yargs')
   .boolean('color').default('color', true).describe('color', 'colorizes output')
   .describe('no-color', 'prevents colorized output')
-  .boolean('json').describe('json', 'provides parseable JSON output (implies --no-colors)')
+  .boolean('json').describe('json', 'provides parseable JSON output (implies --no-color)')
   .boolean('dry-run').describe('dry-run', 'prevents changes to package.json')
   .alias('dry-run', 'n')
   .boolean('ignore-stars').describe('ignore-stars', 'ignore updates to packages that are set to "*"')
@@ -18,7 +18,7 @@ var options = require('yargs')
   .argv;
 
 if (options.check) { options['dry-run'] = true; }
-if (options.json) { options['no-color'] = true; }
+if (options.json) { options.color = false; }
 
 var salita = require('../');
 

index.js
@@ -98,7 +98,6 @@ var createResultTable = function (caption) {
  * The main entry point.
  */
 var salita = function salita(dir, options, callback) {
-  chalk.enabled = !options['no-color'] && !options.json;
   // Package.json.
   var filename = path.join(dir, 'package.json');
   jsonFile(filename).then(function (pkg) {